df477a938042bb50b508a22e2d4f0befb7e90f9e
Anders HK <andhk11@gmail.com>
2021-02-07 00:09:04 +0100

    Added GET for a single bank

--- src/main/kotlin/com/example/demo/controller/BankController.kt
+++ src/main/kotlin/com/example/demo/controller/BankController.kt
@@ -2,13 +2,21 @@ package com.example.demo.controller
 
 import com.example.demo.model.Bank
 import com.example.demo.service.BankService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("api/banks")
 class BankController(private val service: BankService) {
+
+    @ExceptionHandler(NoSuchElementException::class)
+    fun handleNotFound(e: NoSuchElementException): ResponseEntity<String> =
+        ResponseEntity(e.message, HttpStatus.NOT_FOUND)
+
     @GetMapping
     fun getBanks(): Collection<Bank> = service.getBanks()
+
+    @GetMapping("/{accountNumber}")
+    fun getBank(@PathVariable accountNumber: String) = service.getBank(accountNumber)
 }
--- src/main/kotlin/com/example/demo/datasource/mock/MockBankDataSource.kt
+++ src/main/kotlin/com/example/demo/datasource/mock/MockBankDataSource.kt
@@ -7,10 +7,14 @@ import org.springframework.stereotype.Repository
 @Repository
 class MockBankDataSource : BankDataSource {
     val banks = listOf(
-        Bank("1234", 3.0, 17),
+        Bank("1234", 3.14, 17),
         Bank("8080", 5.0, 0),
         Bank("0000", 0.0, 100)
     )
 
     override fun retrieveBanks(): Collection<Bank> = banks
+
+    override fun retrieveBank(accountNumber: String): Bank =
+        banks.firstOrNull { it.accountNumber == accountNumber }
+            ?: throw NoSuchElementException("Could not find a bank with account number $accountNumber")
 }
