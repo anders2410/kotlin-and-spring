e64020f02f066e5ec0fa798d7a5104c60ad36cfc
Anders HK <andhk11@gmail.com>
2021-02-07 00:32:52 +0100

    Added POST for banks

--- src/main/kotlin/com/example/demo/datasource/mock/MockBankDataSource.kt
+++ src/main/kotlin/com/example/demo/datasource/mock/MockBankDataSource.kt
@@ -3,10 +3,11 @@ package com.example.demo.datasource.mock
 import com.example.demo.datasource.BankDataSource
 import com.example.demo.model.Bank
 import org.springframework.stereotype.Repository
+import java.lang.IllegalArgumentException
 
 @Repository
 class MockBankDataSource : BankDataSource {
-    val banks = listOf(
+    val banks = mutableListOf(
         Bank("1234", 3.14, 17),
         Bank("8080", 5.0, 0),
         Bank("0000", 0.0, 100)
@@ -17,4 +18,13 @@ class MockBankDataSource : BankDataSource {
     override fun retrieveBank(accountNumber: String): Bank =
         banks.firstOrNull { it.accountNumber == accountNumber }
             ?: throw NoSuchElementException("Could not find a bank with account number $accountNumber")
+
+    override fun createBank(bank: Bank): Bank {
+        if (banks.any { it.accountNumber == bank.accountNumber }) {
+            throw IllegalArgumentException("Bank with account number ${bank.accountNumber} already exists")
+        }
+
+        banks.add(bank)
+        return bank
+    }
 }
